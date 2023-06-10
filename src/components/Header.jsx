eff3bb59045d76247a80256d4018472258d79d2c
Artemprogrmist1337 <110352581+Artemprogrmist1337@users.noreply.github.com>
2023-06-10 19:42:28 +0200

    add styles css module files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import classes from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header className="header">
+    <header className={classes.header}>
       <img
         src="https://img.freepik.com/vektoren-kostenlos/goldener-vogel-logo-design_1195-336.jpg?w=2000"
         className="img-lol"
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,22 +1,23 @@
 import React from "react";
+import classes from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className="content">
+    <div className={classes.content}>
       <div>
         <img
           src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg"
-          className="schapka"
+          className={classes.schapka}
         ></img>
       </div>
       <div>ava + description</div>
       <div>
         my posts
         <div>New post</div>
-        <div>
-          <div>post 1</div>
-          <div>post 2</div>
-          <div>post 3</div>
+        <div className={classes.posts}>
+          <div className={classes.item}>post 1</div>
+          <div className={classes.item}>post 2</div>
+          <div className={classes.item}>post 3</div>
         </div>
       </div>
     </div>
