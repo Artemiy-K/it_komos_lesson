dbd4d44e1ad61c923abeff3bcf274781fa28e957
Artemprogrmist1337 <110352581+Artemprogrmist1337@users.noreply.github.com>
2023-06-10 18:59:11 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://img.freepik.com/vektoren-kostenlos/goldener-vogel-logo-design_1195-336.jpg?w=2000"
+        className="img-lol"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg"
+          className="schapka"
+        ></img>
+      </div>
+      <div>ava + description</div>
+      <div>
+        my posts
+        <div>New post</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+          <div>post 3</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
