d2fac2d351d215f205da8d65992cde86dbc592b0
Artemprogrmist1337 <110352581+Artemprogrmist1337@users.noreply.github.com>
2023-06-18 15:22:26 +0200

    the avatar was created

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import classes from "./Profile.module.css";
 import MyPosts from "./MyPosts/MyPosts";
 import test_shapka from "../../static/test_shapka.jpg";
+import test_avatar from "../../static/test_avatar.png";
+import test_shapka2 from "../../static/test_shapka2.png";
 
 const Profile = () => {
   return (
     <div className={classes.content}>
       <div className={classes.schapka}>
-        <img src={test_shapka}></img>
+        <img src={test_shapka2}></img>
       </div>
-      <div>ava + description</div>
+      {/* <div>ava + name + description + @id + activity</div> */}
 
+      <div className={classes.avatar}>
+        <img src={test_avatar} />
+      </div>
+
+      <div></div>
       {/* <MyPosts /> */}
     </div>
   );
