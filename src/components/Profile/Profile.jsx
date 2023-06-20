bb25af5a4607f46b0f37b70bbc15c0a86ff02f08
Artemprogrmist1337 <110352581+Artemprogrmist1337@users.noreply.github.com>
2023-06-20 15:48:01 +0200

    first design of pages`s links

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -12,23 +12,62 @@ const Profile = () => {
         <img src={test_shapka2}></img>
       </div>
       {/* <div>ava + name + description + @id + activity</div> */}
-
-      <div className={classes.avatar}>
-        <img src={test_avatar} />
-      </div>
-
-      <div className={classes.nickname}>
-        <h3>Asemai</h3>
-      </div>
-
-      <div className={classes.item_header_content}>
-        <div>
-          <h2 className={classes.user_id}>@user</h2>
+      <div>
+        <div className={classes.avatar}>
+          <img src={test_avatar} />
         </div>
-        <div className={classes.activity}>
-          <span>Active 1 hour</span>
+
+        <div className={classes.nickname}>
+          <h3>Asemai</h3>
+        </div>
+
+        <div className={classes.item_header_content}>
+          <div>
+            <h2 className={classes.user_id}>@user</h2>
+          </div>
+          <div className={classes.activity}>
+            <span>Active 1 hour</span>
+          </div>
+        </div>
+
+        <div className={classes.bp_wrap}>
+          <div className={classes.row}>
+            <ul className={classes.row_of_pages}>
+              <li>
+                <a title="Activity">
+                  <span>Activity</span>
+                </a>
+              </li>
+              <li>
+                <a title="Profile">
+                  <span className={classes.current}>Profile</span>
+                </a>
+              </li>
+              <li>
+                <a title="Friends">
+                  <span>Friends</span>
+                </a>
+              </li>
+              <li>
+                <a title="Groups">
+                  <span>Groups</span>
+                </a>
+              </li>
+              <li>
+                <a title="Forums">
+                  <span>Forums</span>
+                </a>
+              </li>
+              <li>
+                <a title="Media">
+                  <span>Media</span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
+
       {/* <MyPosts /> */}
     </div>
   );
